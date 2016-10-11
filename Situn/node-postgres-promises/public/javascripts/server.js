eaa62f48673bf1a27f1b2b176070395f6343326e
alexandraAguilar <alexandra.aguilar.najera@gmail.com>
2016-10-11 14:59:42 -0700

    tju
    kk

--- Situn/node-postgres-promises/public/javascripts/server.js
+++ Situn/node-postgres-promises/public/javascripts/server.js
@@ -16,7 +16,7 @@ var options = {
 var pgp = require('pg-promise')(options);
 
 //------ CONEXION A LA BASE DE DATOS ------------------
-var connectionString = "pg://postgres:postgres@localhost:5432/BD_SITUN"; // CAMBIAR POR CLAVE DEL POSTGRES DE USTEDES
+var connectionString = "pg://postgres:root@localhost:5432/BD_SITUN"; // CAMBIAR POR CLAVE DEL POSTGRES DE USTEDES
 var db = pgp(connectionString);
 
 
@@ -521,6 +521,5 @@ module.exports = {
   updateTU: updateTU,
   updateTC: updateTC,
   updateTD: updateTD,
-  updateTA: updateTA,
-  getLastTC: getLastTC
+  updateTA: updateTA
 };
