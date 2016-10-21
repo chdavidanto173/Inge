470d953f1e55d685fa7c3b2eec48cd8909ac01d2
JOSE AVALOS <nachoab4@hotmail.com>
2016-10-20 22:15:58 -0600

    Consulta con procedimientos

--- script_SITUN.sql
+++ script_SITUN.sql
@@ -59,3 +59,62 @@ CONSTRAINT pkTA PRIMARY KEY (TA_1, TA_2)
 
 
 insert into TP values ('Grettel','Castro','Cruz','1');
+
+
+
+
+
+------------------------FUNCIONES--------------------------
+---------------------CREAR POR SEPARADO--------------------
+
+-----------------------------------------------------------
+CREATE OR REPLACE FUNCTION post(xn integer) RETURNS void  AS
+$BODY$
+DECLARE
+v_i integer;
+BEGIN insert into x (select td_2 from td where td_1=xn );
+   FOR v_i IN select td_2 from td,x where td.td_1=x.a loop
+       insert into x(select td_2 from td,x where td.td_1=x.a group by td_2,a order by a);
+       end loop;  
+END $BODY$ LANGUAGE 'plpgsql'
+
+----------------------------------------------------------
+
+
+CREATE OR REPLACE FUNCTION pre(xn integer) RETURNS void  AS
+$BODY$
+DECLARE
+v_i integer;
+BEGIN insert into x (select td_1 from td where td_2=xn );
+   FOR v_i IN select td_1 from td,x where td.td_2=x.a loop
+      insert into x(select td_1 from td,x where td.td_2=x.a group by a,td_1 order by a);
+       end loop; 
+END $BODY$ LANGUAGE 'plpgsql'
+
+---------------------------------------------------------
+
+-------------------LLAMADO A FUNCIONES-------------------
+CREATE OR REPLACE FUNCTION final(x integer) RETURNS void  AS
+$BODY$
+DECLARE 
+BEGIN
+create temp table x(a integer);
+perform post(x);
+perform  pre(x);      
+END $BODY$ LANGUAGE 'plpgsql'
+--------------------------------------------------------
+CREATE OR REPLACE FUNCTION Borrado() RETURNS void  AS
+$BODY$
+DECLARE 
+BEGIN
+drop table x;     
+END $BODY$ LANGUAGE 'plpgsql'
+--------------------------------------------------------
+------------EJECUTAR FUNCION ----------------------------
+select final(1)
+-----------CONSULTA FINAL -------------------------------
+--*******************************************************
+select tc_1,tc_2,tc_3,tc_4,tc_5,tc_6,tc_7,tc_8,tc_9,tc_10,tc_11 from tc,x where tc_1=x.a group by tc.tc_1,a order by tc_1;
+
+----------------BOORADO TEMPORAL TABLE------------------------------------
+select Borrado();
